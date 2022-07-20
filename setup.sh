1a70877e007f83a9e55109cada15da79b5097a23
vidutalwar <109663960+vidutalwar@users.noreply.github.com>
2022-07-20 16:49:29 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"21f1004809@student.onlinedegree.iitm.ac.in\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
